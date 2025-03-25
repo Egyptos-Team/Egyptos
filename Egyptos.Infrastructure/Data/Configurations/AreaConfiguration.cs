a3e45a92ae0598dc3c25685a9085ad4d185a1063
Nassar <mohammednassar772003@gmail.com>
2025-03-25 03:24:10 +0200

    Add Configration for three tables area, area type and area images

---
+++ Egyptos.Infrastructure/Data/Configurations/AreaConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Egyptos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Egyptos.Infrastructure.Data.Configurations;
+
+public class AreaConfiguration : IEntityTypeConfiguration<Area>
+{
+    public void Configure(EntityTypeBuilder<Area> builder)
+    {
+        var areas = new List<Area>
+        {
+           new Area{
+                 Id=1,
+                 Name="",
+                 Description="",// biiiiiiiiiiiiiiiiiiiiig
+                 Location="",
+                 Address="",
+                 ImageUrl="",
+                 AreaTypeId=1,
+            },
+           new Area{
+                 Id=2,
+                 Name="",
+                 Description="",// biiiiiiiiiiiiiiiiiiiiig
+                 Location="",
+                 Address="",
+                 ImageUrl="",
+                 AreaTypeId=1,
+           },
+        };
+        builder.HasData();
+    }
+}
---
+++ Egyptos.Infrastructure/Data/Configurations/AreaImagesConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Egyptos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Egyptos.Infrastructure.Data.Configurations;
+
+public class AreaImagesConfiguration : IEntityTypeConfiguration<AreaImage>
+{
+    public void Configure(EntityTypeBuilder<AreaImage> builder)
+    {
+        var areaImages = new List<AreaImage>
+        {
+            new AreaImage
+            {
+                Id=1,
+                Description="",
+                ImageUrl="",
+                AreaId=1
+            },
+            new AreaImage
+            {
+                Id=2,
+                Description="",
+                ImageUrl="",
+                AreaId=1
+            }
+        };
+        builder.HasData();
+    }
+}
