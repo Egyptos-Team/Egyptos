797a70b78c7ff15add5b5849bb16d74642defee3
Nassar <mohammednassar772003@gmail.com>
2025-03-25 03:24:10 +0200

    Add Configration for three tables area, area type and area images

---
+++ Egyptos.Infrastructure/Data/Configurations/AreaTypesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Egyptos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Egyptos.Infrastructure.Data.Configurations;
+
+public class AreaTypesConfiguration : IEntityTypeConfiguration<AreaType>
+{
+    public void Configure(EntityTypeBuilder<AreaType> builder)
+    {
+        var areaTypes = new List<AreaType>
+        {
+            new AreaType
+            {
+                Id=1,
+                Name=""
+            },
+            new AreaType
+            {
+                Id=2,
+                Name=""
+            }
+        };
+        builder.HasData();
+    }
+}
