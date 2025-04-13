2fa398e7bea9fc0d3a189e3bd970462544aa37f7
Nassar <mohammednassar772003@gmail.com>
2025-04-13 20:25:41 +0200

    Edit: edit in items table add new prop area id  for relation between area and item

--- Egyptos.Domain/Entities/Item.cs
+++ Egyptos.Domain/Entities/Item.cs
@@ -7,4 +7,8 @@ public class Item
     public string Description { get; set; } = null!;
     public DateOnly Date { get; set; }
     public string ImageUrl { get; set; } = null!;
+
+    public int AreaId { get; set; }
+
+    public Area Area{ get; set; }
 }
