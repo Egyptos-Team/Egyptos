cf497e9d9bea97ca532e1bec8d8e6abee3a1bd2b
ITcharDO <dfsfv527@gmail.com>
2025-04-20 20:12:58 +0200

    Add : PublicTransport & Trip Configuration

--- Egyptos.Infrastructure/Data/Configurations/AreaTypesConfiguration.cs
+++ Egyptos.Infrastructure/Data/Configurations/AreaTypesConfiguration.cs
@@ -10,41 +10,49 @@ public class AreaTypesConfiguration : IEntityTypeConfiguration<AreaType>
     {
         var areaTypes = new List<AreaType>
         {
+            // Fixed artifacts: 30 1:30
             new AreaType 
             {
                 Id=1,
                 Name="Fixed artifacts"
             },
+            //Archaeological sites: 20 31:50 
             new AreaType 
             {
                 Id=2,
                 Name="Archaeological sites"
             },
+            //Sunken artifacts: 8 71:78
             new AreaType 
             {
                 Id=3,
                 Name="Sunken artifacts"
             },
+            // Safari: 10 61:70
             new AreaType 
             {
                 Id=4,
                 Name="Safari"
             },
+            // Sea and Nile Cruises: 10 51:60
             new AreaType 
             {
                 Id=5,
                 Name="Marine and Nile"
             },
+            // Gblya: 10 81:90
             new AreaType 
             {
                 Id=6,
                 Name="Mountainous"
             },
+            // Ryfya & 6by3ya W Astgmam: 10 91:100
             new AreaType 
             {
                 Id=7,
                 Name="Rural and natural"
             },
+            //Museums: 22 101:122
             new AreaType 
             {
                 Id=8,
--- Egyptos.Infrastructure/Data/Configurations/PublicTransportConfiguration.cs
+++ Egyptos.Infrastructure/Data/Configurations/PublicTransportConfiguration.cs
@@ -13,5 +13,19 @@ public class PublicTransportConfiguration : IEntityTypeConfiguration<PublicTrans
 
         builder.HasIndex(p => p.SerialNumber)
             .IsUnique();
+
+        var PublicTransport = new List<PublicTransport>
+        {
+            new  PublicTransport
+            {
+                Id = 1,
+                Name = "Bus",
+                Capacity = 50,
+                ImageUrl = "PublicTransportmages/1.webp",
+                SerialNumber = 1,
+                IsAvailable = 1
+            },
+        };
+        builder.HasData(PublicTransport);
     }
 }
