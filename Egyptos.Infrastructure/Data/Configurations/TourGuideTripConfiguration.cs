3892db4ed59f47a1c184e0aa083b93004fbb6d59
Nassar <mohammednassar772003@gmail.com>
2025-04-28 23:14:38 +0300

    Add : Data

--- Egyptos.Infrastructure/Data/Configurations/TourGuideTripConfiguration.cs
+++ Egyptos.Infrastructure/Data/Configurations/TourGuideTripConfiguration.cs
@@ -9,5 +9,26 @@ public class TourGuideTripConfiguration : IEntityTypeConfiguration<TourGuideTrip
     public void Configure(EntityTypeBuilder<TourGuideTrip> builder)
     {
         builder.HasKey(x => new { x.TripId, x.TourGuideId });
+
+        var tourGuideTrips = new List<TourGuideTrip>();
+
+        int totalTrips = 122; 
+        int totalTourGuides = 19;
+
+        for (int tripId = 1; tripId <= totalTrips; tripId++)
+        {
+            if(tripId == 79 || tripId == 80 || tripId == 12)
+                continue;
+
+            int tourGuideId = ((tripId - 1) % totalTourGuides) + 1;
+
+            tourGuideTrips.Add(new TourGuideTrip
+            {
+                TripId = tripId,
+                TourGuideId = tourGuideId
+            });
+        }
+
+        builder.HasData(tourGuideTrips);
     }
 }
