0716df97579012880d28c8b96d621bf9d30154f8
ITcharDO <dfsfv527@gmail.com>
2025-05-24 23:30:01 +0300

    Edit  Tour guide image

---
+++ Egyptos.Infrastructure/Data/Migrations/20250524202857_EditTourGuideImage.cs
@@ -0,0 +1,284 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Egyptos.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditTourGuideImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "10eb7637-95f4-45d2-b200-84dbb188ec41",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "249aa6c5-d578-4517-8a42-48f24a7d959e",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "357c8ce0-ed6e-4979-b1f9-5e0f6e90c9a1",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "49a438ba-672d-41d5-97ba-5536ffbec2a6",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "52511e3a-0690-45b0-9e52-02667a473b18",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "5d577849-97d1-4984-bf96-6fee7d08c8be",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "683211b1-1125-42a1-b148-a606a4ca4df3",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6b9b3e85-ce32-4944-9a46-601bb2b40000",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "7c30ae16-67eb-4ff2-858f-70aa79aaa3c4",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8766c317-1eaa-468a-b34d-59790c8eb915",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a0e2f1e7-53e5-416f-b0b0-ca4668563233",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "bf2ee728-59af-45ec-9b70-e5f5e27eef8e",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df2",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df3",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df4",
+                column: "ImageUrl",
+                value: "profiles/22.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "ce47a5ab-df14-416a-b500-9b544fa666a4",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "df2ea217-2e82-48b9-9442-47556c1a9403",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "f89bca4a-8fc9-4259-a46d-22562907c27f",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "fc4df06f-05c2-4751-96c1-8040f2761b7f",
+                column: "ImageUrl",
+                value: "profiles/11.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "10eb7637-95f4-45d2-b200-84dbb188ec41",
+                column: "ImageUrl",
+                value: "profiles/16 Black Chefs Changing Food in America (Published 2019).jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "249aa6c5-d578-4517-8a42-48f24a7d959e",
+                column: "ImageUrl",
+                value: "profiles/MEN - Location - Headshot Photography by Brandon Tabiolo.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "357c8ce0-ed6e-4979-b1f9-5e0f6e90c9a1",
+                column: "ImageUrl",
+                value: "profiles/Before and After_ Transforming Your LinkedIn Profile with AI Headshots.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "49a438ba-672d-41d5-97ba-5536ffbec2a6",
+                column: "ImageUrl",
+                value: "profiles/Why I Mainly Photograph in Portrait Orientation for Actors' Headshots.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "52511e3a-0690-45b0-9e52-02667a473b18",
+                column: "ImageUrl",
+                value: "profiles/Dallas Headshots Photographer - Willie & Kim Photography.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "5d577849-97d1-4984-bf96-6fee7d08c8be",
+                column: "ImageUrl",
+                value: "profiles/468f9794-778b-429f-bea8-b7440810819d.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "683211b1-1125-42a1-b148-a606a4ca4df3",
+                column: "ImageUrl",
+                value: "profiles/df4fb389-64ed-486e-be7d-722771b6e3bc.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6b9b3e85-ce32-4944-9a46-601bb2b40000",
+                column: "ImageUrl",
+                value: "profiles/Friendly Professional Business Headshot.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "7c30ae16-67eb-4ff2-858f-70aa79aaa3c4",
+                column: "ImageUrl",
+                value: "profiles/Rajan Velu.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8766c317-1eaa-468a-b34d-59790c8eb915",
+                column: "ImageUrl",
+                value: "profiles/2edc3141-b4cd-411d-9136-33440bbb1c03.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a0e2f1e7-53e5-416f-b0b0-ca4668563233",
+                column: "ImageUrl",
+                value: "profiles/9c12d50d-5cbe-4c75-8bca-cbc57d504a7b.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "bf2ee728-59af-45ec-9b70-e5f5e27eef8e",
+                column: "ImageUrl",
+                value: "profiles/Gorgeous smile of Alexis Petit.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df2",
+                column: "ImageUrl",
+                value: "profiles/Women in Business.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df3",
+                column: "ImageUrl",
+                value: "profiles/9151a7a4-4fc2-4bb0-a427-f97f4127e661.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "cb3cb7ca-1921-49fd-a11e-600b351b3df4",
+                column: "ImageUrl",
+                value: "profiles/9151a7a4-4fc2-4bb0-a427-f97f4127e661.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "ce47a5ab-df14-416a-b500-9b544fa666a4",
+                column: "ImageUrl",
+                value: "profiles/Mari Juliano.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "df2ea217-2e82-48b9-9442-47556c1a9403",
+                column: "ImageUrl",
+                value: "profiles/Outdoor Portraits — San Francisco Bay Area's Premier Personal Brand and Headshot Photographer _ Alpa.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "f89bca4a-8fc9-4259-a46d-22562907c27f",
+                column: "ImageUrl",
+                value: "profiles/44050f40-662d-408b-9dda-9e3271d7c36e.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "fc4df06f-05c2-4751-96c1-8040f2761b7f",
+                column: "ImageUrl",
+                value: "profiles/82ebaf90-31fe-40ef-8358-99823ce3881d.jpeg");
+        }
+    }
+}
