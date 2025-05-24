7203d36a1123c1e7a9bdbea1d85a995fb92870c2
Raafat Ashraf <raafat.ashraf.17@gmail.com>
2025-05-24 15:46:36 +0300

    edit : booking trip validation

--- Egyptos.Application/Contracts/BookingTrip/BookingTripRequestValidator.cs
+++ Egyptos.Application/Contracts/BookingTrip/BookingTripRequestValidator.cs
@@ -1,8 +1,6 @@
-﻿using Egyptos.Application.Contracts.BookingTourGuide;
-using FluentValidation;
-
+﻿
 namespace Egyptos.Application.Contracts.BookingTrip;
-internal class BookingTripRequestValidator : AbstractValidator<BookingTripRequest>
+public class BookingTripRequestValidator : AbstractValidator<BookingTripRequest>
 {
     public BookingTripRequestValidator()
     {
@@ -19,6 +17,7 @@ internal class BookingTripRequestValidator : AbstractValidator<BookingTripReques
            .WithName(nameof(BookingTripRequest.NumberOfTickets))
            .WithMessage("{PropertyName} must be grater than or equal one "); ;
     }
+
     private bool HasValidDates(BookingTripRequest request)
     {
         return request.NumberOfTickets >= 1;
