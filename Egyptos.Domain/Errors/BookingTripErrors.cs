5b7c67e2573b7895cf930fe05b6331e1892da9ad
ITcharDO <dfsfv527@gmail.com>
2025-05-26 20:43:07 +0300

    Edit Add & Delet Booking Trip

--- Egyptos.Domain/Errors/BookingTripErrors.cs
+++ Egyptos.Domain/Errors/BookingTripErrors.cs
@@ -6,7 +6,7 @@ public class BookingTripErrors
     public static readonly Error TripNotFount = new("BookingTrip.TripNotFount",
         "No trip was found with the given ID", StatusCodes.Status404NotFound);
 
-        public static readonly Error BookingNotFount = new("BookingTrip.BookingNotFount",
+    public static readonly Error BookingNotFount = new("BookingTrip.BookingNotFount",
     "No Booking trip was found with the given Id", StatusCodes.Status404NotFound);
 
     public static readonly Error UserNotBooked = new("BookingTrip.UserNotBooked",
@@ -14,4 +14,8 @@ public class BookingTripErrors
 
     public static readonly Error TripNotBooked = new("BookingTrip.TripNotBooked",
         "There are no reservations for this trip", StatusCodes.Status404NotFound);
+
+    public static readonly Error NotEnoughSeatsAvailable = new("BookingTrip.NotEnoughSeatsAvailable",
+    "You have requested more tickets than available for this trip.",StatusCodes.Status400BadRequest);
+
 }
